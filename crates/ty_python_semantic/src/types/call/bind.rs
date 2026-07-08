f640da7c3022743e4d348e60bc5c760f7673ef5b
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-07-08 11:45:36 -0400

    Remove CompactString compatibility from Name

--- crates/ty_python_semantic/src/types/call/bind.rs
+++ crates/ty_python_semantic/src/types/call/bind.rs
@@ -1373,7 +1373,7 @@ impl<'db> Bindings<'db> {
                                     Some("__name__") => {
                                         overload.set_return_type(Type::string_literal(
                                             db,
-                                            typevar.name(db),
+                                            typevar.name(db).as_str(),
                                         ));
                                     }
                                     Some("__bound__") => {
@@ -2057,7 +2057,7 @@ impl<'db> Bindings<'db> {
                                                 Type::heterogeneous_tuple(
                                                     db,
                                                     names.iter().map(|name| {
-                                                        Type::string_literal(db, *name)
+                                                        Type::string_literal(db, name.as_str())
                                                     }),
                                                 )
                                             }
@@ -2078,10 +2078,9 @@ impl<'db> Bindings<'db> {
                                         {
                                             Type::heterogeneous_tuple(
                                                 db,
-                                                metadata
-                                                    .members
-                                                    .keys()
-                                                    .map(|member| Type::string_literal(db, member)),
+                                                metadata.members.keys().map(|member| {
+                                                    Type::string_literal(db, member.as_str())
+                                                }),
                                             )
                                         } else {
                                             Type::unknown()
@@ -2098,10 +2097,9 @@ impl<'db> Bindings<'db> {
                             if let [Some(ty)] = overload.parameter_types() {
                                 overload.set_return_type(Type::heterogeneous_tuple(
                                     db,
-                                    list_members::all_members(db, *ty)
-                                        .into_iter()
-                                        .sorted()
-                                        .map(|member| Type::string_literal(db, &member.name)),
+                                    list_members::all_members(db, *ty).into_iter().sorted().map(
+                                        |member| Type::string_literal(db, member.name.as_str()),
+                                    ),
                                 ));
                             }
                         }
--- crates/ty_python_semantic/src/types/class/static_literal.rs
+++ crates/ty_python_semantic/src/types/class/static_literal.rs
@@ -1003,7 +1003,7 @@ impl<'db> StaticClassLiteral<'db> {
                     explicit_metaclass_of: class_metaclass_was_from,
                 }
             } else {
-                let name = Type::string_literal(db, class.name(db));
+                let name = Type::string_literal(db, class.name(db).as_str());
                 let bases = Type::heterogeneous_tuple(db, class.explicit_bases(db));
                 let namespace = KnownClass::Dict
                     .to_specialized_instance(db, &[KnownClass::Str.to_instance(db), Type::any()]);
@@ -1649,7 +1649,7 @@ impl<'db> StaticClassLiteral<'db> {
                             false
                         }
                     })
-                    .map(|(name, _)| Type::string_literal(db, name));
+                    .map(|(name, _)| Type::string_literal(db, name.as_str()));
                 Some(Type::heterogeneous_tuple(db, match_args))
             }
             (field_policy @ CodeGeneratorKind::DataclassLike(_), "__weakref__")
@@ -1729,7 +1729,9 @@ impl<'db> StaticClassLiteral<'db> {
                 self.has_dataclass_param(db, field_policy, DataclassFlags::SLOTS)
                     .then(|| {
                         let fields = self.fields(db, specialization, field_policy);
-                        let slots = fields.keys().map(|name| Type::string_literal(db, name));
+                        let slots = fields
+                            .keys()
+                            .map(|name| Type::string_literal(db, name.as_str()));
                         Type::heterogeneous_tuple(db, slots)
                     })
             }
@@ -1779,7 +1781,7 @@ impl<'db> StaticClassLiteral<'db> {
 
         let overloads = frozen_base_fields
             .keys()
-            .map(|field| setattr_signature(Type::string_literal(db, field), Type::Never))
+            .map(|field| setattr_signature(Type::string_literal(db, field.as_str()), Type::Never))
             .chain([setattr_signature(
                 KnownClass::Str.to_instance(db),
                 Type::none(db),
